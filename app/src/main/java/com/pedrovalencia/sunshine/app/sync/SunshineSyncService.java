080ce020b6e161b547928de153f945be8127ad9e
Pedro Valencia <ohmio79@gmail.com>
2014-10-10 06:51:22 +0100

    Adding SyncAdapter

---
+++ app/src/main/java/com/pedrovalencia/sunshine/app/sync/SunshineSyncService.java
@@ -0,0 +1,29 @@
+package com.pedrovalencia.sunshine.app.sync;
+
+/**
+ * Created by pedrovalencia on 10/10/2014.
+ */
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.util.Log;
+
+public class SunshineSyncService extends Service {
+    private static final Object sSyncAdapterLock = new Object();
+    private static SunshineSyncAdapter sSunshineSyncAdapter = null;
+
+    @Override
+    public void onCreate() {
+        Log.d("SunshineSyncService", "onCreate - SunshineSyncService");
+        synchronized (sSyncAdapterLock) {
+            if (sSunshineSyncAdapter == null) {
+                sSunshineSyncAdapter = new SunshineSyncAdapter(getApplicationContext(), true);
+            }
+        }
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return sSunshineSyncAdapter.getSyncAdapterBinder();
+    }
+}
